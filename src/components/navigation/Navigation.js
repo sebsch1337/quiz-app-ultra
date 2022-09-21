2e52e906b4dd55b9e3af0903a739afe957cb0112
Sebastian Scherbes <github@scherbes.de>
2022-09-21 12:06:15 +0200

    add state to Nav component

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,23 +1,47 @@
+import { useState } from "react";
 import "./Navigation.css";
 import bookmarksIcon from "../../img/bookmarks.svg";
 import homeIcon from "../../img/home.svg";
 import profileIcon from "../../img/profile.svg";
 
 const Navigation = () => {
+  const [activeNav, setActiveNav] = useState("home");
+
   return (
     <footer className="footer">
       <nav className="navigation">
-        <div className="navigation__button navigation__button--active">
+        <button
+          onClick={() => setActiveNav("home")}
+          className={
+            activeNav === "home"
+              ? "navigation__button navigation__button--active"
+              : "navigation__button"
+          }
+        >
           <img src={homeIcon} alt="Navigate to home" />
-        </div>
+        </button>
 
-        <div className="navigation__button">
+        <button
+          onClick={() => setActiveNav("bookmarks")}
+          className={
+            activeNav === "bookmarks"
+              ? "navigation__button navigation__button--active"
+              : "navigation__button"
+          }
+        >
           <img src={bookmarksIcon} alt="Navigate to bookmarks" />
-        </div>
+        </button>
 
-        <div className="navigation__button">
+        <button
+          onClick={() => setActiveNav("profile")}
+          className={
+            activeNav === "profile"
+              ? "navigation__button navigation__button--active"
+              : "navigation__button"
+          }
+        >
           <img src={profileIcon} alt="Navigate to profile" />
-        </div>
+        </button>
       </nav>
     </footer>
   );

60332bfa69ec0e3848f5d94afde2ac8f4b37ae4f
Sebastian Scherbes <github@scherbes.de>
2022-09-23 16:33:30 +0200

    fix profile class-attributes

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -3,14 +3,14 @@ import profileImage from "../../img/me.jpg";
 
 const Profile = () => {
   return (
-    <section class="profile">
+    <section className="profile">
       <h1>Hi Sebastian!</h1>
-      <img src={profileImage} alt="A picture of me" class="profile__pic" />
-      <p class="profile__text">
+      <img src={profileImage} alt="A picture of me" className="profile__pic" />
+      <p className="profile__text">
         I don't know what a profile in a quiz game is needed for. But anyways...
         <br />
         <br />
-        <span class="profile__text__cheers">Cheers!</span>
+        <span className="profile__text__cheers">Cheers!</span>
       </p>
     </section>
   );
